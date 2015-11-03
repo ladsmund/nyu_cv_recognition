e60b067ff9378722cf9b5801c97f0b848c868653
Mads <madsclund@gmail.com>
2015-11-03 18:35:48 -0500

    Don't rescale and use multiple sigmas for each lambda

--- generateDescriptor.m
+++ generateDescriptor.m
@@ -4,28 +4,28 @@ images = cell(0);
 images{end+1} = img;
 
 descriptor = [];
-for layerIndex = 1:length(p.filter.layerScale)
-    scale = p.filter.layerScale(layerIndex);
-    sigma = p.filter.layerSigma(layerIndex);
-    newImages = cell(length(images) * length(p.filter.angles),1);
+for layerIndex = 1:p.filter.layers    
+    newImages = cell(length(images),length(p.filter.angles),length(p.filter.sigmas));
 %     fprintf('Number of images in layer: %i\n',length(newImages))
     for imageIndex = 1:length(images)
         image = images{imageIndex};
-        for angleIndex = 1:length(p.filter.angles)
-            angle = p.filter.angles(angleIndex);
-            
-            imgFiltered = morletFilter(image, angle, sigma);
-            img_like = imgFiltered .* conj(imgFiltered);
-            
-            features = extractFeatures(img_like, p);
-            
-            descriptor = [descriptor; features(:)];
-            
-            imgResized = imresize(imgFiltered,scale);
-            newImages{(imageIndex-1)*length(p.filter.angles)+angleIndex} = imgResized;
-        end        
+        for sigmaIndex = 1:length(p.filter.sigmas)
+            sigma = p.filter.sigmas(sigmaIndex);
+            for angleIndex = 1:length(p.filter.angles)
+                angle = p.filter.angles(angleIndex);
+
+                imgFiltered = morletFilter(image, angle, sigma);
+                img_like = imgFiltered .* conj(imgFiltered);
+
+                features = extractFeatures(img_like, p);
+
+                descriptor = [descriptor; features(:)];
+
+                newImages{imageIndex,angleIndex,sigmaIndex} = img_like;
+            end
+        end
     end
-    images = newImages;
+    images = newImages(:);
 end
 
 end
--- get_default_parameters.m
+++ get_default_parameters.m
@@ -1,14 +1,14 @@
 function p = get_default_parameters(numberOfAngles, numberOfSubdivision)
 if nargin < 1
-    numberOfAngles = 2;
+    numberOfAngles = 4;
 elseif nargin < 2
-    numberOfSubdivision = 8;
+    numberOfSubdivision = 4;
 end
 p = struct();
 p.filter = struct();
 p.filter.angles = pi*(1:numberOfAngles)/numberOfAngles;
-p.filter.layerScale = 1./[2,2,2];
-p.filter.layerSigma = [2,2,2];
+p.filter.sigmas = [2,6,10];
+p.filter.layers = 2;
 p.extract = struct();
 p.extract.sub_division = [1,1] * numberOfSubdivision;
 p.extract.sigma = 6;  

2fb5815a98fe537861e22f67987dd1d3b98f0853
Mads <madsclund@gmail.com>
2015-11-03 18:36:32 -0500

    Support multiple sigma parameters for lambdas. Bugfix: Use the given parameters.

--- create_raw_descriptors.m
+++ create_raw_descriptors.m
@@ -3,15 +3,21 @@ input_path = '~/workspace/nyu_cv_recognition_data/train/';
 output_path = '~/workspace/nyu_cv_recognition_data/output/';
 [~,~] = mkdir(output_path);
 
-for sub_devision = [8, 4, 2, 1]
-    for sigma = [1,2,4,8]
-        name = sprintf('sd%i_ang4_lSc2_lSigma%i',sub_devision,sigma);
+
+for sub_division = [1,2,4]
+    for sigmas = [1,2,6]'
+        
+        str_sigmas = ['sigmas' sprintf('-%.0f',sigmas)];
+        str_sub_div = ['subdiv' sprintf('-%.0f',sub_division)];
+        name = sprintf('%s_%s',str_sigmas, str_sub_div);
         disp('***********************')
         disp(name)
         disp('***********************')
-        parameters = get_default_parameters(4,8);
+        parameters = get_default_parameters(4,sub_division);
+        parameters.filter.sigmas = sigmas';
         descriptors_struct = batch_descriptors(input_path, parameters);
         save([output_path name], 'descriptors_struct');
     end
 end
 
+%%

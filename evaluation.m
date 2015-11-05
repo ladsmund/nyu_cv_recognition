d57c990804d340f8cc4062b8f91a8211fdcb0377
Mads <madsclund@gmail.com>
2015-11-05 16:17:22 -0500

    ..

--- evaluation.m
+++ evaluation.m
@@ -14,19 +14,19 @@ input_folder = '~/workspace/nyu_cv_recognition_data/output_train/';
 data_files = dir([input_folder data_file_name]);
 for i = 1:length(data_files)
     data_file_name = data_files(i).name;    
-    input_path = [input_folder data_file_name]    
+    input_path = [input_folder data_file_name];
     
     cfMat =evaluate(input_path, classifier_parameters);
  
-    sensitivity = cfMat(1,1) / sum(cfMat(:,1));
-    specificity = cfMat(2,2) / sum(cfMat(:,2));
 
-    fprintf('%s:\n\tsensitivity=%.4f\n\tspecificity=%.4f\n', output_name,sensitivity, specificity)
-
-    output_name = sprintf('%s_L%i_nfd%i',data_file_name(1:end-4), classifier_parameters.L,classifier_parameters.useNonefaceDist)
+    output_name = sprintf('%s_L%i_nfd%i',data_file_name(1:end-4), classifier_parameters.L,classifier_parameters.useNonefaceDist);
     [~, ~]= mkdir(output_dir);
     output_path = [output_dir output_name '.mat'];
     
+    sensitivity = cfMat(1,1) / sum(cfMat(:,1));
+    specificity = cfMat(2,2) / sum(cfMat(:,2));
+
+    fprintf('%s:\n\tsensitivity=%.4f\n\tspecificity=%.4f\n', output_name,sensitivity, specificity);
 
     save(output_path, 'cfMat');
     
